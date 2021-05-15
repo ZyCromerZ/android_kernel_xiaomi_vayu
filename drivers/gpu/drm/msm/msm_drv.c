639d1fd3b552b9d5dd59a1e5b494ce779c8f8578
ZyCromerZ <neetroid97@gmail.com>
2021-05-15 13:04:37 +0700

    treewide: fix derp after upstream
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -2147,7 +2147,7 @@ static int msm_pdev_remove(struct platform_device *pdev)
 static void msm_pdev_shutdown(struct platform_device *pdev)
 {
 	struct drm_device *ddev = platform_get_drvdata(pdev);
-	struct msm_drm_private *priv = drm ? drm->dev_private : NULL;
+	struct msm_drm_private *priv = ddev ? ddev->dev_private : NULL;
 
 	if (!priv || !priv->kms)
 		return;
